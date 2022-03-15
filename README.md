7c6ce5d8813ee449722b6fba97d51e2481113c3b
Woojin718 <64531055+Woojin718@users.noreply.github.com>
2022-03-15 12:14:45 +0900

    update

--- README.md
+++
@@ -1,2 +0,0 @@
-# skwic_website
- 
--- script.js
+++ script.js
@@ -1,33 +1,35 @@
 $(function(){
-    $('.fuwat').css('visibility','hidden');
-        $(window).scroll(function(){
-        var windowHeight = $(window).height(),
-            topWindow = $(window).scrollTop();
-        $('.fuwat').each(function(){
-        var objectPosition = $(this).offset().top;
-        if(topWindow > objectPosition - windowHeight + 200){
-        $(this).addClass("fuwatAnime");
-        }
-        });
-        });
-    
-        $('.slider').slick({
-            dots: true,
-            slidesToShow: 4,
-            slidesToScroll: 1,
-            responsive:[{
-                breakpoint: 480,
-                settings: {
-                  slidesToShow: 1,
-                  slideToScroll: 1
-                }
-              }],
-              respondTo: 'slider'
-            });
-         $(".more").on("click", function() {
-              $(this).toggleClass("on-click");
-              $(".txt-hide").slideToggle(1000);
-              $("#access").css('margin-top','200px');
-              $(".txt-hide").css('diplay','flex');
-         });
+  $('.fuwat').css('visibility','hidden');
+      $(window).scroll(function(){
+      var windowHeight = $(window).height(),
+          topWindow = $(window).scrollTop();
+      $('.fuwat').each(function(){
+      var objectPosition = $(this).offset().top;
+      if(topWindow > objectPosition - windowHeight + 200){
+      $(this).addClass("fuwatAnime");
+      }
+      });
+      });
+  
+      $('.slider').slick({
+          dots: true,
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          responsive:[{
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slideToScroll: 1
+              }
+            }],
+            respondTo: 'slider'
+          });
+      });
+
+      $(".more").on("click", function() {
+        $(this).toggleClass("on-click");
+        $(".txt-hide").slideToggle(1000);
+        $("#contact").css('margin-top','380px');
+        $(".txt-hide").css('display','flex');
+        $(".more").css('display','none');
         });
